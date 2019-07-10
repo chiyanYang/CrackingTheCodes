f086dca72abeb7e08bf265ea7a0b7e8e363a4e3a
chiyan <52123873+chiyanYang@users.noreply.github.com>
2019-07-10 09:31:34 +0100

    Exercise 2_8 updated, not finished
    Exercise 2_8 updated, not finished

--- CrackingTheCodes/Chapter2/2_8/2_8.cpp
+++ CrackingTheCodes/Chapter2/2_8/2_8.cpp
@@ -5,3 +5,47 @@ void test2_8()
 {
 
 }
+
+sNode* loopDetection(sNode* list)
+{
+	if (list == NULL)
+		return NULL;
+
+	bool isLoop = false;
+	int count = 1;
+	sNode* slow = list;
+	sNode* fast = list;
+	fast = list->getNextNode();
+
+	if (fast == slow)
+		return fast;
+
+	while (fast != NULL)
+	{
+		slow = slow->getNextNode();
+		count++;
+
+		fast = fast->getNextNode();
+		if (fast == slow)
+		{
+			isLoop = true;
+			break;
+		}
+
+		fast = fast->getNextNode();
+		{
+			isLoop = true;
+			break;
+		}
+	}
+
+	if (isLoop)
+	{
+
+	}
+	else
+	{
+		return NULL;
+	}
+
+}
--- CrackingTheCodes/Chapter2/2_8/2_8.h
+++ CrackingTheCodes/Chapter2/2_8/2_8.h
@@ -2,3 +2,4 @@
 #include "../Ch2Common.h"
 
 void test2_8();
+sNode* loopDetection();
