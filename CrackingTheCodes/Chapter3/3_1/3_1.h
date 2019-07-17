ec352073fb9a02789d2c049e58c062f999409e62
chiyan <52123873+chiyanYang@users.noreply.github.com>
2019-07-17 17:57:19 +0100

    Update 3_1.h

--- CrackingTheCodes/Chapter3/3_1/3_1.h
+++ CrackingTheCodes/Chapter3/3_1/3_1.h
@@ -7,7 +7,8 @@ template <typename T> class fixStack;
 template <typename T>
 class fixStack
 {
-	T* index[3];
+	const int numOfStack = 3;
+	T* index[numOfStack];
 
 	struct idx
 	{
@@ -20,19 +21,82 @@ class fixStack
 public:
 	fixStack(int length)
 	{
-		int average = length / 3;
+		int average = length / numOfStack;
+		
 		index[0] = new T[length];
-		index[1] = index[0] + average;
-		index[2] = index[1] + average;
-
-		states[0].curIdx = 0;
+		states[0].curIdx = -1;
 		states[0].length = average;
 
-		states[1].curIdx = 0;
-		states[1].length = average;
+		for (int i = 1; i < numOfStack - 1; i++)
+		{
+			index[i] = index[i - 1] + average;
+			states[i].curIdx = -1;
+			states[i].length = average;
+		}
 
-		states[2].curIdx = 0;
-		states[2].length = length - average * 2;
+		index[numOfStack - 1] = index[numOfStack - 2] + average;
+		states[numOfStack - 1].curIdx = -1;
+		states[numOfStack - 1].length = length - average * 2;
+	}
+
+	bool push(int stackIdx, T data)
+	{
+		if (stackIdx >= numOfStack)
+			return;
+
+		T* nextSpace;
+
+		if (!this.isFull(stackIdx))
+		{
+			nextSpace = index[stackIdx] + states[stackIdx].curIdx + 1;
+			*nextSpace = data;
+			states[stackIdx].curIdx += 1;
+			return true;
+		}
+
+		return false;
+	}
+
+	void pop(int stackIdx)
+	{
+		if (stackIdx >= numOfStack)
+			return;
+
+		if (!this.isEmpty(stackIdx))
+		{
+			states[stackIdx].curIdx--;
+		}
+	}
+
+	T peek(int stackIdx)
+	{
+		if (stackIdx >= numOfStack)
+			return;
+
+		if (!this.isEmpty(stackIdx))
+		{
+			states[stackIdx].curIdx--;
+			T* curData = index[stackIdx] + states[stackIdx].curIdx;
+			return *curData;
+		}
+
+		return NULL;
+	}
+
+	bool isEmpty(int stackIdx)
+	{
+		if (states[stackIdx].curIdx == -1)
+			return true;
+
+		return false;
+	}
+
+	bool isFull(int stackIdx)
+	{
+		if (states[stackIdx].curIdx == states[stackIdx].length - 1)
+			return true;
+
+		return false;
 	}
 
 	~fixStack()
