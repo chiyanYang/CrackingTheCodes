e53fa790cc3f75070fa34da04bc35867f493421e
chiyan <52123873+chiyanYang@users.noreply.github.com>
2019-07-22 18:49:28 +0100

    Added init to 3_2
    Added init to 3_2

--- CrackingTheCodes/Chapter3/3_2/3_2.h
+++ CrackingTheCodes/Chapter3/3_2/3_2.h
@@ -2,3 +2,12 @@
 #include "../Ch3Common.h"
 
 void test3_2();
+
+
+template <typename T> class stackWithMin;
+
+template <typename T>
+class stackWithMin : public vector<T>
+{
+
+};
--- CrackingTheCodes/Common.h
+++ CrackingTheCodes/Common.h
@@ -6,6 +6,7 @@
 #include <sstream>
 #include <ctype.h>
 #include <vector>
+#include <stack>
 #include <algorithm>    // std::swap
 #include <unordered_set>
 #include <thread> // For sleep function
