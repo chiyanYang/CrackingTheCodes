de66133ca80aea21629daa098e88c74b34c47cbe
chiyan <52123873+chiyanYang@users.noreply.github.com>
2019-07-23 17:55:55 +0100

    Update 3_4.h

--- CrackingTheCodes/Chapter3/3_4/3_4.h
+++ CrackingTheCodes/Chapter3/3_4/3_4.h
@@ -2,3 +2,44 @@
 #include "../Ch3Common.h"
 
 void test3_4();
+
+template <typename T> class MyQueue;
+
+template <typename T>
+void testFunction(MyQueue<T>& myQueue);
+
+template <typename T>
+class MyQueue
+{
+private:
+	vector<T> v1;
+	vector<T> v2;
+
+public:
+	MyQueue() {}
+
+	bool isEmpty()
+	{
+		return false;
+	}
+
+	bool enqueue(T data)
+	{
+		return true;
+	}
+
+	T dequeue()
+	{
+		return NULL;
+	}
+
+	T* front()
+	{
+		return NULL;
+	}
+
+	T* rear()
+	{
+		return NULL;
+	}
+};
