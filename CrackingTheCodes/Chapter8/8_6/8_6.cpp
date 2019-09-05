ecb004ea4e55196611b66ea1321b0a5a780fe19e
chiyan <52123873+chiyanYang@users.noreply.github.com>
2019-09-05 09:33:18 +0100

    working on 8_6

--- CrackingTheCodes/Chapter8/8_6/8_6.cpp
+++ CrackingTheCodes/Chapter8/8_6/8_6.cpp
@@ -3,4 +3,52 @@
 // Towers of Hanoi
 void test8_6()
 {
+	stack<int> towers[3];
+	fillDestinationTower(towers[0], 5);
+	printTotwers(towers);
+
+	towersOfHanoi(towers, 0, 2, 1);
+}
+
+void towersOfHanoi(stack<int> towers[3], int source, int des, int middle)
+{
+	if (towers[source].size() == 0 && towers[middle].size() == 0)
+	{
+		return;
+	}
+
+	int value = towers[source].top();
+	towers[source].pop();
+
+	towers[middle].push(value);
+
+	towersOfHanoi(towers, int source, int des, int middle)
+
+}
+
+void fillDestinationTower(stack<int>& tower, int num)
+{
+	for (int i = num; i >= 0; i--)
+	{
+		tower.push(i);
+	}
+}
+
+void printTotwers(stack<int> towers[3])
+{
+	cout << endl;
+
+	for (int i = 0; i < 3; i++)
+	{
+		stack<int> copied = towers[i];
+		for (int j = 0; j < copied.size; j++)
+		{
+			cout << copied.top() << " ";
+			copied.pop();
+		}
+
+		cout << endl;
+	}
+
+	cout << endl;
 }
--- CrackingTheCodes/Chapter8/8_6/8_6.h
+++ CrackingTheCodes/Chapter8/8_6/8_6.h
@@ -2,3 +2,7 @@
 #include "../Ch8Common.h"
 
 void test8_6();
+
+
+void fillDestinationTower(stack<int>& tower, int num);
+void printTotwers(stack<int> towers[3]);
